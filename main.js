45ef4786e2f8821b145b879741bdece81a0772f9
fiona-hogg-audacia <111744738+fiona-hogg-audacia@users.noreply.github.com>
2022-09-06 13:53:40 +0100

    update: todays work

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
+            product: 'Socks', // updated data value //
+            brand: 'Vue Mastery',
+            description: 'Nice and fluffy keeping your toes toasty!',
+            selectedVaraint: 0,
+            url: 'https://www.google.co.uk/',
+            
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyeseter'],
+            sizes: ['small', 'medium', 'large', 'XLarge'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+              ]
+        }
+        
+    },
+    methods: {
+        addToCart(){
+            this.cart +=1
+        },
+        removeCart(){
+            this.cart -=1
+        },
+        updateVariant(index){
+            this.selectedVariant = index
+            
+        }
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() { 
+            return this.variants[this.selectedVaraint].image
+        },
+        inStock() { 
+            return this.variants[this.selectedVaraint].image
+        }
+    }
+})
